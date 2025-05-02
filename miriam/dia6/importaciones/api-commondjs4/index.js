729b7fa9c319b49e3bc94a4c0d8a3bb58994e939
Miriam2205 <meramlh221@alumnos.cei.es>
2025-05-02 22:50:33 +0200

    importacion

--- miriam/dia6/importaciones/api-commondjs4/index.js
+++ miriam/dia6/importaciones/api-commondjs4/index.js
@@ -1,6 +1,4 @@
-
-
 console.clear()
 console.log('Iniciando api-commondjs4')
 
-const  { suma, resta, multiplicacion, division, potencia, raiz} = require ('./funciones')
+const  { suma, resta, multiplicacion, division, potencia, raiz} = require('./funciones')
