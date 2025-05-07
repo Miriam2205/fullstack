57ed84863c81b3e571c9ad34d061f22a24bea715
Miriam2205 <meramlh221@alumnos.cei.es>
2025-05-07 18:52:38 +0200

    apis

--- miriam/dia7/api1/controller.js
+++ miriam/dia7/api1/controller.js
@@ -1,49 +1,114 @@
 
 //METODO GET
-const getAlumnos = ( req, res, next ) => {
-    res.json('Haciendo get en /alumnos')
+const getAlumnos = ( error, req, res, next ) => {
+    try {
+        res.json('Haciendo get en /alumnos')
+    } catch (error) {
+        next(Error)
+    }
 }
 
-//Buscar mediante id
+    //Buscar mediante id
 const getAlumnosById = (req, res, next) => {
-    const { _id } = req.params
-    res.json(`Buscando mediante el ID ${_id}`)
+    try {
+        const { _id } = req.params
+        res.json(`Buscando mediante el ID ${_id}`)
+    } catch (error) {
+        next(Error)
+    }
+    
 }
 
-//Buscar mediante nombre 
+    //Buscar mediante nombre 
 const getAlumnosNombre = (req, res, next) => {
-    const {_nombre} = req.params
-    res.json(`Buscando mediante nombre ${_nombre}`)
+    try {
+        const {_nombre} = req.params
+        res.json(`Buscando mediante nombre ${_nombre}`)
+    } catch (error) {
+        next(Error)
+    }
+    
 }
-//Buscar mediante aprobado
+    //Buscar mediante aprobado
 const getAlumnosAprobado = (req, res, next ) => {
-    const {_aprobado} = req.params
-    res.json(`Buscando aprobados ${_aprobado}`)
+    try {
+        const {_aprobado} = req.params
+        res.json(`Buscando aprobados ${_aprobado}`)
+    } catch (error) {
+        next(Error)
+    }
 }
 
-//Buscar valor múltiple:  nombre y aprobado
+    //Buscar valor múltiple:  nombre y aprobado
 const getAlumnosByNombreAndAprobado = (req, res, next) => {
-    const { nombre , aprobado } = req.params
-    res.json(`Buscando multiple ${nombre} y ${aprobado}`)
+    const { _nombre , _aprobado } = req.params
+    res.json(`Buscando multiple ${_nombre} y ${_aprobado}`)
 }
 
 //METODO POST
 const postAlumnos = (req, res, next ) => {
-    const {nombre, edad, curso } = req.body
-
-    res.json(`Añadiendo el alumno ${nombre} años y estudia ${curso}`)
+    res.json(`Añadiendo post en alumnos`)
+}
+//Buscar por id
+const postAlumnosById = (req, res, next) => {
+    const {_id} = req.params
+    res.json(`Añadiendo post en alumnos ${_id}`)
+}
+//Buscar por nombre 
+const postAlumnosNombre = (req, res, next) => {
+    const {_nombre} = req.params
+    res.json(`Buscando nombre alumno ${_nombre}`)
+}
+//Buscar por aprobado 
+const postAlumnosAprobado = (req, res, next) => {
+    const{_aprobado} = req.params
+    res.json(`Buscando aprobados ${_aprobado}`)
+}
+//Buscamos por nombre y aprobado
+const postAlumnosByNombreAndAprobado = (req, res, next) => {
+    const {_nombre, _aprobado} = req.params
+    res.json(`Buscando nombres${_nombre} y aprobados${_aprobado} `)
 }
 
+
+//METODO POST
+//Buscar alumnos
 const putAlumnos = (req, res, next) => {
-    const {_id, ...datos} = req.body
-    res.json(datos)
+    res.json(`Buscando alumnos `)
+}
+//Buscamos por id
+const putAlumnosById = (req, res, next) => {
+    const {_id} = req.params
+    res.json(`Buscando alumnos por Id ${_id}`)
+}
+//Buscamos por nombre
+const putAlumnosNombre = (req, res, next) => {
+    const {_nombre} = req.params
+    res.json(`Buscando nombre${_nombre}`)
+}
+//Buscamos por aprobado
+const putAlumnosAprobado = (req, res, next) => {
+    const {_aprobado} = req.params
+    res.json (`Buscando aprobados${_aprobado}`)
+}
+//Buscamos por Nombre y aprobado
+const putAlumnosByNombreAndAprobado = (req, res, next) => {
+    res.json(`Buscando nombres y aprobados `)
 }
 
+
+
+
+
+//METODO PATCH
 const patchAlumnos = (req, res, next) => {
     const { _id, ...datos} = req.body
     res.json(datos)
 }
 
+
+
+//METODO DELETE
 const deleteAlumnos = (req, res, next) => {
     const {_id} = req.params
     res.json(`Haciendo delete en /alumnos con el id ${_id}`)
@@ -55,9 +120,17 @@ module.exports = {
     getAlumnosById,
     getAlumnosNombre,
     getAlumnosAprobado,
+    getAlumnosByNombreAndAprobado,
     postAlumnos,
+    postAlumnosById,
+    postAlumnosNombre,
+    postAlumnosAprobado,
+    postAlumnosByNombreAndAprobado,
     putAlumnos,
+    putAlumnosById,
+    putAlumnosNombre,
+    putAlumnosAprobado,
     patchAlumnos,
     deleteAlumnos,
-    getAlumnosByNombreAndAprobado
+    
 }
--- miriam/dia7/api1/index.js
+++ miriam/dia7/api1/index.js
@@ -2,7 +2,8 @@ console.clear()
 console.log('Iniciando Proyecto api1')
 
 const express = require ('express')
-const {getAlumnos ,getAlumnosNombre, getAlumnosAprobado, postAlumnos, putAlumnos, patchAlumnos, deleteAlumnos, getAlumnosById, getAlumnosByNombreAndAprobado} = require('./controller')
+const {getAlumnos, getAlumnosById, getAlumnosNombre, getAlumnosAprobado,getAlumnosByNombreAndAprobado,postAlumnos, postAlumnosById,postAlumnosNombre,postAlumnosAprobado, postAlumnosByNombreAndAprobado, putAlumnos,putAlumnosById, patchAlumnos, deleteAlumnos, putAlumnosNombre, putAlumnosAprobado} = require('./controller')
+const { middleware404, middleware500 } = require('./middlewares')
 
 const app = express()
 
@@ -14,19 +15,48 @@ const app = express()
     //Metodos
     app.get('/alumnos', getAlumnos)
     //Buscamos mediante id
-    app.get('/alumnos/_id/:_id', getAlumnosById)
+    app.get('/alumnos/id/:_id', getAlumnosById)
     //Buscamos mediante nombre
     app.get('/alumnos/nombre/:_nombre', getAlumnosNombre)
     //Buscamos mediante aprobado
     app.get('/alumnos/aprobado/:aprobado',getAlumnosAprobado )
-
     //Buscamos mediante nombre y aprobado
     app.get(`/alumnos/multiples/:nombre/:aprobado`, getAlumnosByNombreAndAprobado)
 
+
+
     app.post('/alumnos', postAlumnos)
+    //Buscar por id
+    app.post('/alumnos/id/:_id', postAlumnosById)
+    //Buscamos por nombre
+    app.post('/alumnos/nombre/:_nombre', postAlumnosNombre)
+    //Buscamos por aprobado 
+    app.post('/alumnos/aprobado/:_aprobado', postAlumnosAprobado)
+    //Buscamos por nombre y aprobado 
+    app.post('/alumnos/multiples/:_nombre/:_aprobado', postAlumnosByNombreAndAprobado)
+
+
+
+
     app.put('/alumnos', putAlumnos)
-    app.patch('/alumnos', patchAlumnos)
+    //Buscamos por id
+    app.put('/alumnos/_id/:_id', putAlumnosById)
+    //Buscamos por nombre
+    app.put('/alumnos/_nombre/:_nombre', putAlumnosNombre)
+    //Buscamos por aprobado
+    app.put('/alumnos/_aprobado/:_aprobado', putAlumnosAprobado)
+    //Buscamos por nombre y aprobado 
+    
+
+
+
+
+    app.patch('/alumnos/_nombre/:_nombre', patchAlumnos)
     app.delete('/alumnos/:_id', deleteAlumnos)
 
+
+
+    app.use(middleware404)
+    app.use(middleware500)
 
     app.listen( 3000, ()=> console.log('Iniciando api rest 1'))
