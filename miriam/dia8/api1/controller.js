94b531880c1e139d4a091af0e64dbd9fe537dcfc
Miriam2205 <meramlh221@alumnos.cei.es>
2025-05-12 00:18:35 +0200

    api

--- miriam/dia8/api1/controller.js
+++ miriam/dia8/api1/controller.js
@@ -1,272 +1,87 @@
-
-//METODO GET
-const getAlumnos = ( req, res, next ) => {
+getAlumnos = (req, res, next)=> {
     try {
-        res.status(200).json({message: `Buscando alumnos`, data: []})
+        res.json(`Haciendo get en /alumnos`)
     } catch (error) {
-        next(Error)
+        next(error)
+    }
+}
+getAlumnosById = (req, res, next)=> {
+    try {
+        const {_id}= req.params
+        res.json(`Buscando mediante id ${_id}`)
+    } catch (error) {
+        next(error)
+    }
+}
+getAlumnosByNombre = (req, res, next)=> {
+    try {
+        const {nombre}= req.params
+        res.json(`Buscando al alumno con nombre ${nombre}`)
+    } catch (error) {
+        next(error)
+    }
+}
+getAlumnosByAprobado = (req, res, next)=> {
+    try {
+        const {aprobado} = req.params
+        res.json(`Buscando alumnos aprobado ${aprobado}`)
+    } catch (error) {
+        
+    }
+}
+getAlumnosByNombreAndAprobado = (req, res, next)=>{
+    try {
+        const {nombre, aprobado} = req.params
+        res.json(`Buscando nombre${nombre} y aprobado${aprobado}`)
+    } catch (error) {
+        next(error)
     }
 }
 
-    //Buscar mediante id
-const getAlumnosById = (req, res, next) => {
-    try {
-        const { _id } = req.params
-        res.status(200).json( {message: `Buscando by id${_id}`, data: []} )
-    } catch (error) {
-        next(Error)
-    }
-    
-}
-
-    //Buscar mediante nombre 
-const getAlumnosNombre = (req, res, next) => {
-    try {
-        const {_nombre} = req.params
-        res.status(200).json({message: `Buscando by nombre ${_nombre}`, data: []})
-    } catch (error) {
-        next(Error)
-    }
-    
-}
-    //Buscar mediante aprobado
-const getAlumnosAprobado = (req, res, next ) => {
-    try {
-        const {_aprobado} = req.params
-        res.status(200).json ({message: `Buscando aprobados ${_aprobado}`, data: []})
-    } catch (error) {
-        next(Error)
-    }
-}
-
-    //Buscar valor múltiple:  nombre y aprobado
-const getAlumnosByNombreAndAprobado = (req, res, next) => {
-    try {
-        const { _nombre , _aprobado } = req.params
-        res.status(200).json({message: `Buscando nombre ${_nombre} y aprobado${_aprobado}`, data: []})
-    } catch (error) {
-        next(Error)
-    }
-    
-}
-
-//METODO POST
-const postAlumnos = (req, res, next ) => {
+postAlumnos = (req, res, next)=> {
     try {
         const {nombre, edad, curso} = req.body
-        res.status(201).json({message: ` Añadiendo ${nombre} con edad ${edad} y que estudia ${curso}`, data: []})
+       res.json(`Añadiendo al alumno ${nombre}, de edad ${edad} y que estudia ${curso}`) 
     } catch (error) {
-        next(Error)
+        next(error)
     }
 }
-//Buscar por id
-const postAlumnosById = (req, res, next) => {
+putAlumnos = (req, res, next)=>{
     try {
-        const {nombre, edad, curso} = req.body
-        res.status(200).json({message: `Buscando alumnos by ${_id}Id`, data: []})
+        const {_id, ...datos}= req.body
+        res.json(datos)
     } catch (error) {
-        next(Error)
+        next(error)
     }
-   
 }
-//Buscar por nombre 
-const postAlumnosNombre = (req, res, next) => {
-    try {
-        const {_nombre} = req.params
-        res.status(200).json({message: `Buscando alumnos by nombre ${_nombre}`, data: []})
-    } catch (error) {
-        next(Error)
-    }
-   
-}
-//Buscar por aprobado 
-const postAlumnosAprobado = (req, res, next) => {
-    try {
-        const{_aprobado} = req.params
-        res.status(200).json({message: `Buscando ${_aprobado} `, data: [] })
-    } catch (error) {
-        next(Error)
-    }
-    
-}
-//Buscamos por nombre y aprobado
-const postAlumnosByNombreAndAprobado = (req, res, next) => {
-    try {
-        const {_nombre, _aprobado} = req.params
-        res.json(200).json({message: `Buscando nombres${_nombre} y aprobados${_aprobado} `, data: []})
-    } catch (error) {
-        next(Error)
-    }
-
-}
-
-
-//METODO POST
-const putAlumnos = (req, res, next) => {
+patchAumnos=(req, res,next)=>{
     try {
         const {_id, ...datos} = req.body
         res.json(datos)
     } catch (error) {
-        next(Error)
+        next(error)
     }
 }
-//Buscamos por id
-const putAlumnosById = (req, res, next) => {
+deleteAlumnos = (req, res,next)=> {
     try {
-        const {_id} = req.params
-        res.json(`Buscando alumnos por Id ${_id}`)
+        const {_id}= req.params
+        res.json(`Haciendo delete en /alumnos con el id ${_id}`)
     } catch (error) {
-        next(Error)
-    }
-
-}
-//Buscamos por nombre
-const putAlumnosNombre = (req, res, next) => {
-    try {
-        const {_nombre} = req.params
-        res.json(`Buscando nombre${_nombre}`)
-    } catch (error) {
-        next(Error)
-    }
-
-}
-//Buscamos por aprobado
-const putAlumnosAprobado = (req, res, next) => {
-    try {
-        const {_aprobado} = req.params
-        res.json (`Buscando aprobados${_aprobado}`)
-    } catch (error) {
-        next(Error)
-    }
-
-}
-//Buscamos por Nombre y aprobado
-const putAlumnosByNombreAndAprobado = (req, res, next) => {
-    try {
-        const{_nombre, _aprobado} = req.params
-        res.json(`Buscando nombres ${_nombre} y aprobados ${_aprobado} `)
-    } catch (error) {
-        next(Error)
+        next(error)
     }
 }
-
-
-//METODO PATCH
-const patchAlumnos = (req, res, next) => {
-    try {
-        const {_id, ...datos} = req.body
-        res.json(datos)
-    } catch (error) {
-        next(Error)
-    }
-}
-const patchAlumnosById = (req, res, next) => {
-    try {
-        const {_id} = req.params
-        res.json(`Buscando id ${_id}`)
-    } catch (error) {
-        next(Error)
-    }
-    
-}
-const patchAlumnosNombre = (req, res, next) => {
-    try {
-        const {_nombre} = req.params
-        res.json(`Buscando Nombre ${_nombre}`)
-    } catch (error) {
-        next(Error)
-    }
-
-}
-const patchAlumnosAprobado = (req, res, next) => {
-    try {
-        const{_aprobado} = req.params
-        res.json(`Buscando alumnos aprobados ${_aprobado}`)
-    } catch (error) {
-        next(Error)
-    }
-}
-
-const patchAlumnosByNombreAndAprobado = (req, res, next) => {
-   try {
-        const {_nombre, _aprobado} = req.params
-        res.json(`Buscando nombre ${_nombre} y aprobados ${_aprobado}`)
-   } catch (error) {
-        next(Error)
-   }
-
-}
-
-//METODO DELETE
-const deleteAlumnos = (req, res, next) => {
-    try {
-        res.json(`Buscamos alumnos`)
-    } catch (error) {
-        next(Error)
-    }
-}
-const deleteAlumnosById = (req, res, next) => {
-    try {
-        const {_id} = req.params
-        res.json(`Buscando el id ${_id} `)
-    } catch (error) {
-        next(Error)
-    }
-
-}
-
-const deleteAlumnosNombres = (req, res, next) => {
-    try {
-        const {_nombre} = req.params
-        res.json(`Buscar nombre de alumnos ${_nombre}`)
-    } catch (error) {
-        next(Error)
-    }
-
-}
-const deleteAlumnosAprobado = (req, res, next) => {
-    try {
-        const {_aprobado} = req.params
-        res.json(`Buscando alumnos aprobados ${_aprobado}`)
-    } catch (error) {
-        next(Error)
-    }
-
-}
-const deleteAlumnosByNombreAndAprobado = (req, res, next) => {
-    try {
-        const {_nombre, _aprobado} = req.params
-        res.json(`Buscando nombres ${_nombre} y aprobados ${_aprobado} `)
-    } catch (error) {
-        next(Error)
-    }
-
-}
-
-module.exports = {
+module.exports ={
     getAlumnos,
     getAlumnosById,
-    getAlumnosNombre,
-    getAlumnosAprobado,
+    getAlumnosByNombre,
+    getAlumnosByAprobado,
     getAlumnosByNombreAndAprobado,
-    postAlumnos,
-    postAlumnosById,
-    postAlumnosNombre,
-    postAlumnosAprobado,
-    postAlumnosByNombreAndAprobado,
-    putAlumnos,
-    putAlumnosById,
-    putAlumnosNombre,
-    putAlumnosAprobado,
-    putAlumnosByNombreAndAprobado,
-    patchAlumnos,
-    patchAlumnosById,
-    patchAlumnosNombre,
-    patchAlumnosAprobado,
-    patchAlumnosByNombreAndAprobado,
     deleteAlumnos,
-    deleteAlumnosById,
-    deleteAlumnosNombres,
-    deleteAlumnosAprobado,
-    deleteAlumnosByNombreAndAprobado
+    postAlumnos,
+    putAlumnos,
+    patchAumnos,
+    
 }
+
+
+
--- miriam/dia8/api1/index.js
+++ miriam/dia8/api1/index.js
@@ -1,17 +1,30 @@
 console.clear()
-console.log('Iniciando api3')
+console.log(`Iniciando api1`)
 
-
-const express = require ( 'express')
-const {router} = require('./router')
-const { PORT  } = require('dotenv').config().parsed
+const express = require('express')
 const cors = require('cors')
-const app = express()
 
+const {getAlumnos,getAlumnosById, getAlumnosByNombre,getAlumnosByAprobado, getAlumnosByNombreAndAprobado,deleteAlumnos, postAlumnos,putAlumnos,patchAumnos}= require('./controller')
+const{middleware404, middleware500}= require('./middlewares')
+const app = express()
 
     app.use(cors())
     app.use(express.json())
-    app.use(express.urlencoded({extendend: false}))
-    app.use(router)
-    
-    app.listen ( 3000, () => console.log('Express iniciada'))
+    app.use(express.urlencoded({extended: false}))
+
+    app.get (`/alumnos`, getAlumnos)
+    app.get(`/alumnos/_id/:_id`,getAlumnosById)
+    app.get('/alumnos/nombre/:nombre', getAlumnosByNombre)
+    app.get('/alumnos/aprobado/:aprobado', getAlumnosByAprobado)
+    app.get('/alumnos/multiples/:nombre/:aprobado', getAlumnosByNombreAndAprobado)
+    app.post('/alumnos', postAlumnos)
+    app.put('/alumnos', putAlumnos)
+    app.patch('/alumnos', patchAumnos)
+    app.delete('/alumnos/:_id', deleteAlumnos)
+
+    app.use(middleware404)
+    app.use(middleware500)
+
+
+app.listen(3000, ()=> console.log(`Iniciando api1`))
+
