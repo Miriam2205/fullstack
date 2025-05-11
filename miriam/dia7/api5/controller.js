d791f4c079eb7ab2c12b36f916b9f007a63d73df
Miriam2205 <meramlh221@alumnos.cei.es>
2025-05-11 12:21:22 +0200

    api dia7

---
+++ miriam/dia7/api5/controller.js
@@ -0,0 +1,45 @@
+getAlumnos = (req, res, next)=> {
+try {
+    res.json(`Haciendo get en /alumnos`)
+} catch (error) {
+    next(error)
+}
+}
+
+postAlumnos = (req, res, next)=> {
+    try {
+        res.json(`Haciendo post en /alumnos`)
+    } catch (error) {
+        next(error)
+    }
+}
+
+putAlumnos=(req, res, next)=> {
+    try {
+        res.json(`Haciendo put en /alumnos`)
+    } catch (error) {
+        next(error)
+    }
+}
+
+patchAlumnos=(req, res, next)=> {
+    try {
+        res.json(`Haciendo patch en /alumnos`)
+    } catch (error) {
+        next(error)
+    }
+}
+deleteAlumnos=(req, res,next)=> {
+    try {
+        res.json(`Haciendo delete en /alumnos`)
+    } catch (error) {
+        next(error)
+    }
+}
+module.exports = {
+    getAlumnos,
+    postAlumnos,
+    putAlumnos,
+    patchAlumnos,
+    deleteAlumnos
+}
---
+++ miriam/dia7/api5/index.js
@@ -0,0 +1,24 @@
+console.clear()
+console.log(`Iniciando api5`)
+
+const express = require('express')
+const {getAlumnos, postAlumnos,putAlumnos,patchAlumnos,deleteAlumnos} = require('./controller')
+
+const app = express()
+
+    app.use(express.json())
+    app.use(express.urlencoded({extended: false}))
+
+
+    app.get('/alumnos', getAlumnos)
+    app.post('/alumnos', postAlumnos)
+    app.put('/alumnos', putAlumnos)
+    app.patch('/alumnos', patchAlumnos)
+    app.delete('/alumnos', deleteAlumnos)
+
+
+
+
+
+
+app.listen(3000, ()=> console.log(`Iniciando api5`))
